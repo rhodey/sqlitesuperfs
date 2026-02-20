94b5793a24ecde662d148e6f4572793992d0c3dd
rhodey <mike@rhodey.org>
2026-02-20 11:29:30 -0500

    more raft tests

--- test/7raft.js
+++ test/7raft.js
@@ -24,7 +24,7 @@ const toObj = (buf) => {
   return JSON.parse(buf.toString('utf8'))
 }
 
-test('test append, open, close, new', async (t) => {
+test('raft append, open, close, new', async (t) => {
   empty(DIR)
   let log = new FsLog(`${DIR}/`, 'test')
   t.teardown(() => log.close())
@@ -77,6 +77,96 @@ test('test append, open, close, new', async (t) => {
   t.equal(seq, 4n, 'seq = 4')
   t.equal(log.seq, 4n, 'seq = 4')
   t.deepEqual(toObj(log.head), data, 'head = data')
+  t.end()
+})
 
+test('raft append one, close, open, append', async (t) => {
+  empty(DIR)
+  const log = new FsLog(`${DIR}/`, 'test')
+  t.teardown(() => log.close())
+
+  await log.del()
+  await log.open()
+
+  t.equal(log.seq, -1n, 'seq = -1')
+  t.equal(log.head, null, 'head = null')
+
+  let data = { a: 1 }
+  let seq = await log.append(toBuf(data))
+  t.equal(seq, 0n, 'seq = 0')
+  t.equal(log.seq, 0n, 'seq = 0')
+  t.deepEqual(toObj(log.head), data, 'head = data')
+  await log.close()
+
+  await log.open()
+  t.equal(log.seq, 0n, 'seq = 0 again')
+  t.deepEqual(toObj(log.head), data, 'head = data again')
+
+  data = { b: 2 }
+  seq = await log.append(toBuf(data))
+  t.equal(seq, 1n, 'seq = 1')
+  t.equal(log.seq, 1n, 'seq = 1')
+  t.deepEqual(toObj(log.head), data, 'head = data')
+  t.end()
+})
+
+test('raft rollback first', async (t) => {
+  const rollbackCb = (seq) => {
+    if (seq === 0n) { throw new Error('test roll') }
+  }
+
+  const opts = { rollbackCb }
+  const log = new FsLog(`${DIR}/`, 'test', opts)
+  t.teardown(() => log.close())
+
+  await log.del()
+  await log.open()
+
+  t.equal(log.seq, -1n, 'seq = -1')
+  t.equal(log.head, null, 'head = null')
+
+  try {
+    const data = { a: 1 }
+    await log.append(toBuf(data))
+    t.fail('no error thrown')
+  } catch (err) {
+    t.ok(err.message.includes('test roll'), 'error thrown')
+  }
+
+  t.equal(log.seq, -1n, 'seq = -1 again')
+  t.equal(log.head, null, 'head = null again')
+  t.end()
+})
+
+test('raft rollback second', async (t) => {
+  const rollbackCb = (seq) => {
+    if (seq === 1n) { throw new Error('test roll') }
+  }
+
+  const opts = { rollbackCb }
+  const log = new FsLog(`${DIR}/`, 'test', opts)
+  t.teardown(() => log.close())
+
+  await log.del()
+  await log.open()
+
+  const data = { a: 1 }
+  const seq = await log.append(toBuf(data))
+  t.equal(seq, 0n, 'seq = 0')
+  t.equal(log.seq, 0n, 'seq = 0')
+  t.deepEqual(toObj(log.head), data, 'head = data')
+
+  try {
+    await log.append(toBuf({ b: 2 }))
+    t.fail('no error thrown')
+  } catch (err) {
+    t.ok(err.message.includes('test roll'), 'error thrown')
+  }
+
+  await log.close()
+  await log.open()
+  t.pass('restart ok')
+  t.equal(log.seq, 0n, 'seq = 0 again')
+  t.deepEqual(toObj(log.head), data, 'head = data again')
   t.end()
 })
